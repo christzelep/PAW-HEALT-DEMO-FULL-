fb5b8a70a168e4891b6e4ce2528ed067de20e15b
christzelep <trendinggadgets23@gmail.com>
2025-06-24 21:32:16 +0300

    Update service-worker.js

--- your-repo/service-worker.js
+++ your-repo/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pawhealth-v2'; // Αλλαγή έκδοσης για cache busting
+const CACHE_NAME = 'pawhealth-final-v1';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -11,23 +11,36 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
 
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => Promise.all(
-      cacheNames.map(cacheName => {
-        if (!cacheWhitelist.includes(cacheName)) {
-          return caches.delete(cacheName);
-        }
-      })
-    ))
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
